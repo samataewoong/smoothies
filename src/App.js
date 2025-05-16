ef4e5015fe799e3f6bf2bc747dd45143b20906eb
LEE TAEWOONG <ltwng99@naver.com>
2025-05-16 15:29:15 +0900

    last

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import Update from "./pages/Update";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <nav>
         <h1>Supa Smoothies1</h1>
         <Link to="/">Home</Link>&nbsp;
         <Link to="/create">Create New Smooothie</Link>
+        <a href="https://www.daum.net">다음사이트</a>
+        <a href="/smootihes">홈</a>
       </nav>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
